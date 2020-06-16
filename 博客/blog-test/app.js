3598b077ce3462b078e60841a00961fe0ca5799e
zy751 <52131032+zy751@users.noreply.github.com>
2020-06-16 11:15:25 +0800

    Update app.js

--- 博客/blog-test/app.js
+++ 博客/blog-test/app.js
@@ -1,20 +1,20 @@
 const  express=require('express')
-const Exec=require('./db/Mysql')
-const body =require('body-parser')
-const cookieP =require('cookie-parser')
+const Exec=require('./db/Mysql')//导入数据库
+const body =require('body-parser')//解析请求参数
+const cookieP =require('cookie-parser')//解析cookie
 const ejs=require('ejs')
+app.engine('html',ejs.renderFile)//使得可以使用html代码直接渲染
 const app=express()
 
 
-app.engine('html',ejs.renderFile)
 
-app.set('views','views')
+app.set('views','views')//视图代码存放路径
 app.use(body.urlencoded({extended:false}))
-app.use(cookieP())
+app.use(cookieP())//app实例使用上面导入的
 app.use('/public',express.static('public'))
 
 app.use(require('./router/user'))
-app.use(require('./router/blog'))
+app.use(require('./router/blog'))//导入编写的路由
 
 exec=new Exec()
 
