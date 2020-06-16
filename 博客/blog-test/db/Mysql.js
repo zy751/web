0c811dc4425de57bbcc7b592a3b7ec44df7c2b43
zy751 <52131032+zy751@users.noreply.github.com>
2020-06-16 11:28:58 +0800

    Update Mysql.js

--- 博客/blog-test/db/Mysql.js
+++ 博客/blog-test/db/Mysql.js
@@ -1,14 +1,14 @@
 var  {MYSQL_Conf} =require('../config/db')
 var mysql=require('mysql')
 const conn=mysql.createConnection(MYSQL_Conf)
-conn.connect()
+conn.connect()//连接数据库
 
-const pageSize=4
+const pageSize=4//规定一页的显示内容
 
-class Exec {
+class Exec {//创建数据库类
     constructor() {
     }
-    getPageSum(){
+    getPageSum(){//获取数据可分的页数
         var sql=`select count(id) as sum from blogs`
         var promise=new Promise((res,rej)=>{
             conn.query(sql,null,(err,data)=>{
@@ -17,7 +17,7 @@ class Exec {
         })
         return promise
     }
-    getPage(par){
+    getPage(par){//返回指定页数内容
         var sql=`select * from blogs limit ?,?`
         var promise=new Promise((res,rej)=>{
             conn.query(sql,par,(err,data)=>{
@@ -26,15 +26,15 @@ class Exec {
         })
         return promise
     }
-    addBlog(par){
-        var findsql=`select * from blogs where title='${par[0]}'`
-        var sql='insert into blogs(title,content,createtime,author) values (?,?,?,?)'
+    addBlog(par){//增加博客
+        var findsql=`select * from blogs where title='${par[0]}'`//查询语句
+        var sql='insert into blogs(title,content,createtime,author) values (?,?,?,?)'//增加语句
         var promise=new Promise((res,rej)=>{
-            conn.query(findsql,null,(err,data)=>{
+            conn.query(findsql,null,(err,data)=>{//先查看新增博客主题是否存在
                 if (data[0]){
                     res(false)
                 }else{
-                    conn.query(sql,par,(err,data)=>{
+                    conn.query(sql,par,(err,data)=>{//不存在就新增
                         if (data.affectedRows>0){
                             res(true)
                         }else {
@@ -47,7 +47,7 @@ class Exec {
         })
         return promise
     }
-    findBlogT(par){
+    findBlogT(par){//根据标题查找博客
             var sql='select * from blogs where title  =?'
             var promise=new Promise((res,rej)=>{
                 conn.query(sql,par,(err,data)=>{
@@ -56,7 +56,7 @@ class Exec {
             })
         return promise
     }
-    findBlogK(par){
+    findBlogK(par){//根据关键词查找博客
         var sql='select * from blogs where  title like ? or content like ? limit ?,?'
         var promise=new Promise((res,rej)=>{
             conn.query(sql,par,(err,data)=>{
@@ -65,7 +65,7 @@ class Exec {
         })
         return promise
     }
-    findBlogM(par){
+    findBlogM(par){//查找自己的博客
         var sql=`select * from blogs where author=? order by createtime`
         var promise=new Promise((res,rej)=>{
             conn.query(sql,par,(err,data)=>{
@@ -75,7 +75,7 @@ class Exec {
         return promise
     }
 
-    delBlog(par){
+    delBlog(par){//删除博客
         var sql='delete from blogs where title=?'
         var promise=new Promise((res,req)=>{
             conn.query(sql,par,(err,data)=>{
@@ -88,7 +88,7 @@ class Exec {
         })
         return promise
     }
-    updateBlog(par){
+    updateBlog(par){//更新博客
         var sql='update blogs set content=? ,createtime=?,author=? where title=?'
         var promise=new Promise((res,rej)=>{
             conn.query(sql,par,(err,data)=>{
@@ -101,7 +101,7 @@ class Exec {
         })
         return promise
     }
-    findUser(par) {
+    findUser(par) {//查找用户信息
         var sql='select * from users where username=? and password=?'
         var promise=new Promise((res,rej)=>{
             conn.query(sql,par,(err,data)=>{
@@ -110,11 +110,11 @@ class Exec {
         })
         return promise
     }
-    addUser(par) {
-        var findsql=`select * from users where username='${par[0]}'`
+    addUser(par) {//新增用户
+        var findsql=`select * from users where username='${par[0]}'`//先查询用户是否存在
         var promise=new Promise((res,rej)=>{
             conn.query(findsql,null,(err,data)=>{
-                if(!data[0]){
+                if(!data[0]){//不存在新增
                     var sql='insert into users(username,password,realname) values (?,?,?)'
                     conn.query(sql,par,(err,data)=>{
                         if(err){
@@ -133,7 +133,7 @@ class Exec {
         })
         return promise
     }
-    delUser(par) {
+    delUser(par) {//删除用户
         var sql='delete from users where username=?'
         conn.query(sql,par,(err,data)=>{
             if(err){
@@ -148,7 +148,7 @@ class Exec {
             }
         })
     }
-    updateUser(par) {
+    updateUser(par) {//更新用户信息
         var sql='update users set username=? ,password=? where username=? and password=?'
         var promise=new Promise((res,rej)=>{
             conn.query(sql,par,(err,data)=>{
