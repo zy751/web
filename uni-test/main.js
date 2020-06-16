3c0d1b424bbc5979559fdda9e09dd4b86626f43f
zy751 <52131032+zy751@users.noreply.github.com>
2020-06-16 11:54:02 +0800

    Update main.js

--- uni-test/main.js
+++ uni-test/main.js
@@ -3,7 +3,7 @@ import App from './App'
 import common from './common/common.js'
 
 Vue.config.productionTip = false
-Vue.prototype.$common=common
+Vue.prototype.$common=common//使得可以直接使用在common里的全局变量
 App.mpType = 'app'
 
 const app = new Vue({
